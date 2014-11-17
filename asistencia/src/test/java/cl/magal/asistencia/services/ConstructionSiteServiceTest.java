6fef46968eaba8188eac35e9fe3f70b1a4d40110
Pablo <pcarreno@adeptagestion.com>
2014-11-16 21:42:45 -0300

    Incomplete - task : lista de usuarios

--- asistencia/src/test/java/cl/magal/asistencia/services/ConstructionSiteServiceTest.java
+++ asistencia/src/test/java/cl/magal/asistencia/services/ConstructionSiteServiceTest.java
@@ -1,7 +1,6 @@
 package cl.magal.asistencia.services;
 
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
