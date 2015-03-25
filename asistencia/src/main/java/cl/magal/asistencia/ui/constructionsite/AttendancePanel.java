af51febdce9c4995acdbf0da9ba81ec08cbe8b15
Pablo <pcarreno@adeptagestion.com>
2015-03-25 10:59:00 -0300

    fix panel

--- asistencia/src/main/java/cl/magal/asistencia/ui/constructionsite/AttendancePanel.java
+++ asistencia/src/main/java/cl/magal/asistencia/ui/constructionsite/AttendancePanel.java
@@ -236,6 +236,9 @@ public class AttendancePanel extends Panel implements View {
 			attendanceGrid.removeColumn("attendanceId");
 		if(attendanceGrid.getColumn("date") != null )
 			attendanceGrid.removeColumn("date");
+		if(attendanceGrid.getColumn("marksAsList") != null )
+			attendanceGrid.removeColumn("marksAsList");
+		
 
 		attendanceGrid.setColumnOrder("laborerConstructionSite.activeContract.jobCode","jornalPromedio","d1","d2","d3","d4","d5","d6","d7","d8","d9","d10","d11","d12","d13","d14","d15","d16"
 				,"d17","d18","d19","d20","d21","d22","d23","d24","d25","d26","d27","d28","d29","d30","d31");
