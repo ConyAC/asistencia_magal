640dbb533cc0ec769e66a8a372a1ba2bea7a7c86
Pablo <pcarreno@adeptagestion.com>
2015-06-14 21:59:46 -0400

    20	Agregar campo de vigencia de examen previsional (fecha) 27	Cargar comunas y ciudades Fixes en dependents

--- asistencia/src/main/java/cl/magal/asistencia/entities/Laborer.java
+++ asistencia/src/main/java/cl/magal/asistencia/entities/Laborer.java
@@ -25,6 +25,7 @@ import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.persistence.UniqueConstraint;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 
@@ -123,8 +124,13 @@ public class Laborer implements Serializable {
 //    @Column(name = "contractId")
 //    private Integer contractId;
     
+    @Column(name = "validity_pension_review")
+    @Temporal(TemporalType.DATE)
+    private Date validityPensionReview;
+    
     @Column(name="dependents")
-    private int dependents;
+    @Min(value=0,message="No puede tener menos de 0 cargas")
+    private Integer dependents;
     
     @Column(name="town")
     private String town;
@@ -247,10 +253,10 @@ public class Laborer implements Serializable {
 	}
 
 	public Integer getDependents() {
-		return dependents;
+		return dependents == null ? 0 : dependents;
 	}
 
-	public void setDependents(int dependents) {
+	public void setDependents(Integer dependents) {
 		this.dependents = dependents;
 	}
 
@@ -408,7 +414,15 @@ public class Laborer implements Serializable {
 
 	public void setBank(Bank bank) {
 		this.bank = bank;
-	}    
+	}
+
+	public Date getValidityPensionReview() {
+		return validityPensionReview;
+	}
+
+	public void setValidityPensionReview(Date validityPensionReview) {
+		this.validityPensionReview = validityPensionReview;
+	}
 
 	@Override
     public boolean equals(Object object) {
