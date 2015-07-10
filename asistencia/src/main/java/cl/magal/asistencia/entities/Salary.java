5d62f78918fdaad7edac216e4b58f6d766984b72
Constanza <constanza.aravenac@gmail.com>
2015-07-09 22:38:35 -0400

    se me paso una s

--- asistencia/src/main/java/cl/magal/asistencia/entities/Salary.java
+++ asistencia/src/main/java/cl/magal/asistencia/entities/Salary.java
@@ -124,7 +124,7 @@ public class Salary implements Serializable {
 		getForceSalary();
 	}
 	
-	public void setAdvancePaymentConfiguration(AdvancePaymentConfigurations api){s
+	public void setAdvancePaymentConfiguration(AdvancePaymentConfigurations api){
 		if(this.supleCalculator == null || this.salaryCalculator == null )
 			throw new RuntimeException("Es necesario que el objeto de calculo sea distinto a null");
 		supleCalculator.setSupleTable(api);
